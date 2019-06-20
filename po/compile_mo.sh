ee03f526e9e6fed083ae3ddad7f1b04284c9ebc6
Piyush Aggarwal <piyushaggarwal002@gmail.com>
2019-06-20 09:33:59 +0530

    develop stuff into master now

--- po/compile_mo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-for lang in *; do
-   if [ -d $lang ]; then
-      msgfmt -c --statistics --verbose -o $lang/swish.mo $lang/swish.po
-   fi
-done
--- thirdparty/taskdialog98/res/make_icons_include.bat
+++
@@ -1,4 +0,0 @@
-xxd -i TaskDlgArrowHot.ico >> icons.h
-xxd -i TaskDlgArrowNormal.ico >> icons.h
-xxd -i TaskDlgChevronLess.ico >> icons.h
-xxd -i TaskDlgChevronMore.ico >> icons.h
